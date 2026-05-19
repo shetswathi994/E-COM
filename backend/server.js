99202693ec0de719d8843f97d5751ec09c06c89c
shetswathi994 <shetswathi994@gmail.com>
2026-05-19 19:54:22 +0530

    fixed cloudinary import case

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import express from 'express'
 import cors from 'cors'
 
 import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/Cloudinary.js'
+import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productroute.js'
 
